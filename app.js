17d425f88a67d3e82adbedcf37f6812cae9d181f
Sara Haynes <sjhaynes@gmail.com>
2019-05-05 17:01:43 -0700

    Sum and Multiply Question 3

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ function multiply(a, b) {//eslint-disable-line
 
 // }
 
-testMultiply(5, 9);
 
 // function multiply(a, b) { //eslint-disable-line
 
@@ -63,11 +62,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  // sum(c);
+  // multiply(c);
+var outputSum = sum(a, b);
+var outputExtraSum = sum(c, outputSum[0]);
+var outputMultiply = multiply(a, b);
+var outputExtraMultiply = multiply(c, outputMultiply[0]);
+var messageThirdElement =  a + ' and ' + b + ' and ' + c + ' sum to ' + outputExtraSum[0] + '.';
+var messageFourthElement = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + outputExtraMultiply[0] + '.';
+return [outputExtraSum[0], outputExtraMultiply[0], messageThirdElement, messageFourthElement];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
