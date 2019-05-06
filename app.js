84ae3356085629bb878bdd6b291f0e04afb6a2f9
Sara Haynes <sjhaynes@gmail.com>
2019-05-05 21:49:35 -0700

    sumArray Question 4

--- app.js
+++ app.js
@@ -93,12 +93,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  var a = sumArr[0];
+  var b = sumArr[1];
+  var c = sumArr[2]; 
+  var firstArraySum = sum(a, b);
+  var completeArrayOutput = sum(c, firstArraySum[0]);
+  var message = a + ',' + b + ',' + c + ' was passed in as an array of numbers, and ' + completeArrayOutput[0] + ' is their sum.'; 
+  return [completeArrayOutput[0], message];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -113,13 +120,19 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+//////////////////////////////////////////////////////////////////////////////////////////
+// function multiplyArray(multArr) { //eslint-disable-line
 
-}
+// firstArrayOutput = sumArray[2,3,4];
+// firstArrayMultiplied = mulitiply(sumArray[0]);
+// var message = 'The numbers 2,3,4 have a product of ' + firstArrayMultiplied + '.';
+// return [firstArrayMultiplied[0], message];
 
-// Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+// }
 
+// // Here is the test for multiplyArray(); uncomment it to run it
+//  testMultiplyArray(testArray);
+//////////////////////////////////////////////////////////////////////////////////////////////
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
